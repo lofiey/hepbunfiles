2798967adab5122970f4500c3a2f4c66e5c8e5bc
lofiey <42893670+lofiey@users.noreply.github.com>
2020-07-04 01:04:31 +0800

    Add files via upload

---
+++ f7deat.js
@@ -0,0 +1,11 @@
+//Comment
+$('.avatar-image-container').addClass('rounded-circle');
+$('.comment-actions').children("span").children("a").addClass('btn btn-danger');
+$('.comment-actions').children("a").addClass('btn btn-info');
+$('.comment').addClass('list-group-item m-2');
+$('#comments').addClass('card mx-2');
+$('#comments').children("h4").addClass('card-header');
+//navbar
+$('.nav-item').children("span").addClass('p-5 text-light');
+$('#btnSidebar').on('click',function(){$('.sidebar').toggleClass('d-none')});
+$('.navbar-toggler').on('click',function(){$('#brand').toggleClass('d-none');$('#menu').toggleClass('col-12')});
